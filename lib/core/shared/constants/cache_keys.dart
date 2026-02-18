7c24a3d2dc95a7540d35ae0dadca0de5db591962
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-18 14:01:13 -0300

    ⚙️ config: add feed routes and cache keys

--- lib/core/shared/constants/cache_keys.dart
+++ lib/core/shared/constants/cache_keys.dart
@@ -2,4 +2,5 @@ class CacheKeys {
   static const String accessToken = 'access_token';
   static const String refreshToken = 'refresh_token';
   static const String onboardingCompleted = 'onboarding_completed';
+  static const String feedFilters = 'feed_filters';
 }
