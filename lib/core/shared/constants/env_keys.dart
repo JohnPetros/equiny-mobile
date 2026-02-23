56dfa944315439042c0de2c040f937c08913c12b
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-23 13:23:31 -0300

    ⚙️ config: align env/cache keys and bootstrap websocket session

--- lib/core/shared/constants/env_keys.dart
+++ lib/core/shared/constants/env_keys.dart
@@ -1,7 +1,6 @@
 class EnvKeys {
-  static const String equinyRestServiceUrl = 'EQUINY_REST_SERVICE_URL';
-  static const String equinyWebsocketServiceUrl =
-      'EQUINY_WEBSOCKET_SERVICE_URL';
+  static const String equinyRestUrl = 'EQUINY_REST_URL';
+  static const String equinyWebsocketUrl = 'EQUINY_WEBSOCKET_URL';
   static const String supabaseStorageBucket = 'SUPABASE_STORAGE_BUCKET';
   static const String supabaseKey = 'SUPABASE_KEY';
   static const String supabaseUrl = 'SUPABASE_URL';
