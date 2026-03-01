2e8062ae76a1375d447012b2a0d98955b00f038c
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-03-01 20:16:54 -0300

    🏷️ type: update auth and profiling DTOs

--- lib/core/auth/dtos/jwt_dto.dart
+++ lib/core/auth/dtos/jwt_dto.dart
@@ -1,8 +1,5 @@
-import 'package:equiny/core/profiling/dtos/entities/owner_dto.dart';
-
 class JwtDto {
   final String accessToken;
-  final OwnerDto? owner;
 
-  JwtDto({required this.accessToken, this.owner});
+  JwtDto({required this.accessToken});
 }
